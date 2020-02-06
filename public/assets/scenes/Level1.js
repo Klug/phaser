cca5dcb55b62aaa2a5181669b636df8c02d3cc7f
Klug <42929550+Klug@users.noreply.github.com>
2020-02-06 16:24:15 +0100

    zone. like, when you touch it, youre made the lvl.

--- public/assets/scenes/Level1.js
+++ public/assets/scenes/Level1.js
@@ -27,8 +27,10 @@ export default class Level1 extends Phaser.Scene {
 
 	create() {
 		// //IMAGE
-		// this.star = this.add.image(100,250, 'star'); //Add Image Params = x, y, key
-		// this.star.setScale(2); //Skaliert das Bild
+		this.star = this.add.image(0,0, 'star'); //Add Image Params = x, y, key
+		this.star.setScale(2, 2); //Skaliert das Bild
+        this.star.setOrigin(0, 0); //setzt den startpunkt nicht in der mitte, sondern oben links
+        this.star1 = this.add.image(0,0, 'star').setScale(2, 2).setOrigin(0, 0)//image shortcut
 		// // console.log(this.star.originY); //Consolenausgabe mit originalgröße des Bildes
 		// this.star.setAlpha(1, 0.5, 0.5, 0.2); //Setzt die Helligkeit des Bildes. Lichteffekte möglich
         //
@@ -66,55 +68,89 @@ export default class Level1 extends Phaser.Scene {
         // this.fatboy.flipX = true;
         // this.fatboy.anims.play('right', true);
 
-        //SHAPES
-        this.square = this.add.rectangle(100, 200, 150, 150, 0Xff0000);
-        this.square.setStrokeStyle(0, 0Xff0000, 1);
-        //circle
-        this.circle = this.add.circle(100, 200, 50, 0Xffff00);
-        this.circle.setStrokeStyle();
+        // //SHAPES
+        // this.square = this.add.rectangle(100, 200, 150, 150, 0Xff0000);
+        // this.square.setStrokeStyle(0, 0Xff0000, 1);
+        // //circle
+        // this.circle = this.add.circle(100, 200, 50, 0Xffff00);
+        // this.circle.setStrokeStyle();
+        //
+        // //TEXT
+        // this.gameTitle = this.add.text(200, 200, 'Hey this is my Title',  {
+        //     fonyFamily: 'Courier',
+        //     fontSize: '30px',
+        //     fontStyle: '',
+        //     backgroundColor: null,
+        //     color: '#fff',
+        //     stroke: '#ff0',
+        //     strokeThickness: 2,
+        //     shadow: {
+        //         offsetX: 3,
+        //         offsetY: 3,
+        //         color: '#fff',
+        //         blur: 1,
+        //         stroke: false,
+        //         fill: true
+        //     },
+        //     align: 'center', //left/center/right
+        // });
+        // //googleFont
+        // WebFont.load({
+        //     google: {
+        //         families: ['Fredericka the Great']
+        //     },
+        //     active: () => {
+        //         this.add
+        //             .text(150, 20, 'Google Font', {
+        //             fontFamily: 'Fredericka the Great',
+        //             fontSize: 50,
+        //             color: '#fff'
+        //         })
+        //             .setShadow(2, 2, '#333', 2, false, true);
+        //     }
+        // });
+        // //change Text constantly in update
+        // this.gameTitle.setText('Street Fighter ' + 2);
+        // this.version = 0;
+        //
+        // //GROUP
+        // this.stars = this.add.group({
+        //     key: 'star'gt,
+        //     frame: [0,1,2,3,4], //elementmenge
+        //     setXY: { //changes every step
+        //         x: 100,
+        //         y: 100,
+        //         stepX: 100,
+        //         stepY: 0
+        //     },
+        //     setScale: {
+        //         x: 1,
+        //         y: 1,
+        //         stepX: 1,
+        //         stepY: 1
+        //     }
+        // });
+        //
+        // //CONTAINERS
+        // this.allStars = this.add.container(0, 0);
+        // var star1  = this.add.sprite(0, 0, 'star');
+        // var star2  = this.add.sprite(50, 50, 'star');
+        // var star3  = this.add.sprite(150, 150, 'star');
+        // this.allStars.add(star1);
+        // this.allStars.add(star2);
+        // this.allStars.add(star3);
+        // this.allStars.setX(20);
+        // this.allStars.setY(20);
 
-        //TEXT
-        this.gameTitle = this.add.text(200, 200, 'Hey this is my Title',  {
-            fonyFamily: 'Courier',
-            fontSize: '30px',
-            fontStyle: '',
-            backgroundColor: null,
-            color: '#fff',
-            stroke: '#ff0',
-            strokeThickness: 2,
-            shadow: {
-                offsetX: 3,
-                offsetY: 3,
-                color: '#fff',
-                blur: 1,
-                stroke: false,
-                fill: true
-            },
-            align: 'center', //left/center/right
-        });
-        //googleFont
-        WebFont.load({
-            google: {
-                families: ['Fredericka the Great']
-            },
-            active: () => {
-                this.add
-                    .text(150, 50, 'Google Font', {
-                    fontFamily: 'Fredericka the Great',
-                    fontSize: 50,
-                    color: '#fff'
-                })
-                    .setShadow(2, 2, '#333', 2, false, true);
-            }
-        });
-        //change Text constantly in update
-        this.gameTitle.setText('Street Fighter ' + 2);
-        this.version = 0;
+		//ZONE
+		this.endZone = this.add.zone(200, 200, 50, 50);
+        this.physics.world.enable(this.endZone);
+        this.endZone.body.setAllowGravity(false);
 	}
 	update(time, delta) {
 
         //change Text constantly in update
-        this.gameTitle.setText('Street Fighter ' + this.version);
-        this.version += 1;
+        // this.gameTitle.setText('Street Fighter ' + this.version);
+        // this.version += 1;
     }
 }
