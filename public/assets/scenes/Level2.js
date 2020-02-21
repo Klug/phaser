42dab788bcc09d94368cd6818369fe3f7f62cffd
Klug <42929550+Klug@users.noreply.github.com>
2020-02-21 13:54:34 +0100

    basics with tweens(object animations)

--- public/assets/scenes/Level2.js
+++ public/assets/scenes/Level2.js
@@ -19,7 +19,7 @@ export default class Level2 extends Phaser.Scene {
 	create() {
 		// this.dude = this.add.sprite(100, 200, 'dude').setInteractive().setDepth(2);
 
-		this.star = this.add.sprite(500, 200, 'star').setScale(8, 8).setInteractive().setDepth(1);
+		// this.star = this.add.sprite(500, 200, 'star').setScale(8, 8).setInteractive().setDepth(1);
 
 		//Keyboard INPUT
 		// this.input.enabled = true;
@@ -92,17 +92,43 @@ export default class Level2 extends Phaser.Scene {
 		// this.physics.add.existing(this.dude, 0); //1=static; 0=dynamic(react to world) -> contra not working with setMass
 		// this.dude.body.setMass(25); //workaround
 
-		this.player1 = this.physics.add.sprite(100, 0, 'dude').setScale(3,3).setOrigin(0, 0).setOffset(0, 8);
+		//add dude inline to Physics
+		this.player1 = this.physics.add.sprite(100, 300, 'dude').setScale(3,3).setOrigin(0, 0).setOffset(0, 8);
 		this.player1.setSize(this.player1.body.width - 2, this.player1.body.height - 5, false);
-		console.log(this.player1);
+		// console.log(this.player1);
 
-		this.player2 = this.physics.add.sprite(250, 0, 'dude');
-		this.player2.setMass(25);
+		this.player2 = this.physics.add.sprite(200, 150, 'dude');
 
 		//World bound
 		this.physics.world.setBoundsCollision();
 		this.player1.setCollideWorldBounds(true); //hold player 1 ingame
-		this.physics.world.setBounds(300, 0, 400, 300);
+		this.player2.setCollideWorldBounds(true);
+		// this.physics.world.setBounds(0, 0, this.game.config.width, this.game.config.height); //set the world manuel
+
+		//collider between player 1 and 2
+		// this.physics.add.collider(this.player1, this.player2, () => {
+		// 	console.log('scored a point');
+		// });
+
+		//player1/2 can overlap
+		// this.physics.add.overlap(this.player1, this.player2, () => {
+		// 	console.log('scored a point');
+		// });
+		//
+		// this.player1.setImmovable();
+		// console.log(this.player1.body.blocked); //console log to see wich side is blocked
+		// console.log(this.player1.body.touching); //to see which side player2 is touching player1
+
+		//tweens => object animation from a to b like a bullet
+		var tween = this.tweens.add({
+			targets: this.player1,
+			x: 400, //'+=100'
+			y: 300, //'+=100'
+			ease: 'Linear', //'Cubic', 'Elastic', 'Bounce', 'Back'...
+			duration: 2000,
+			repeat: -1,
+			yoyo: true
+		});
 	}
 
 	update(time, delta) {
@@ -129,6 +155,14 @@ export default class Level2 extends Phaser.Scene {
 		// 	this.dude.setY(this.dude.y + 2);
 		// }
 
-		//Mouse
+		//check if player1 died by reaching the world down collision
+		// if(this.player1.body.blocked.down) {
+		// 	console.log('DIED');
+		// }
+
+		//check if player2 fell on palyer1
+		// if(this.player1.body.touching.up) {
+		// 	console.log('player2 fell on player1');
+		// }
     }
 }
